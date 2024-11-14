85638091ad6638f3d5615ee6ac86954e55cc1a50
Your Name <amanda.salsabilla1785@gmail.com>
2024-11-14 09:22:15 +0700

    Aku-Noob

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid p-5 bg-success text-white text-center">
+  <h1>Form Antrian Pasien</h1>
+  <p>Selamat Datang di Website Programer Noob !!!</p> 
+</div>
+  
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Daftar Antrian</h3>
+      <p><h6>Form daftar</h6></p>
+      <button><a href='daftar_antrian.php'>Daftar</a></button>
+    </div>
+    <div class="col-sm-4">
+      <h3>Tambah Antrian</h3>
+      <p><h6>Form Antrian</h6></p>
+      <button><a href='tambah_antrian.php'>Daftar</a></button>
+    </div>
+
+    <div class="col-sm-4">
+      <h3>Ubah Status</h3>        
+      <p><h6>Form Ubah Status</h6></p>
+      <button><a href='ubah_status.php'>Daftar</a></button>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ lib/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "rpl12345";
+    $dbname = "rumahsakit";
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Koneksi berhasil!";
+    }  catch (PDOException $e) {
+        echo "Koneksi gagal:" . $a->getMassage();
+        
+    }
+
+?>
